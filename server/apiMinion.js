b00fbc536f2595f95d370c9c14c6222e13e6ef04
Joshua Simon Thomas <joshua_jst95@hotmail.com>
2022-03-20 00:31:55 +0800

    Add API for works under Minions, and enable tests for it

--- server/apiMinion.js
+++ server/apiMinion.js
@@ -3,8 +3,10 @@ const { is } = require('express/lib/request');
 const minionRouter = express.Router();
 const db = require('./db');
 const modelMinions = 'minions';
+const modelWork = 'work';
 //initialize db
 minions = db.getAllFromDatabase(modelMinions);
+works = db.getAllFromDatabase(modelWork);
 
 minionRouter.get("/", (req, res, next) => {
     res.send(db.getAllFromDatabase(modelMinions));
@@ -48,6 +50,67 @@ minionRouter.put(`/:minionId`, (req, res, next) => {
 minionRouter.delete(`/:minionId`, (req, res, next) => {
     const id = req.params.minionId;
     const isDeleted = db.deleteFromDatabasebyId(modelMinions, id);
+    if(isDeleted) { res.status(204).send() };
+});
+
+// work API
+minionRouter.param('workId', (req, res, next, id) => {
+    const getWork = works[id];
+    if(getWork) {
+        next()
+    } else {
+        res.status(404).send("Work is not found");
+    }
+})
+
+minionRouter.get('/:minionId/work', (req, res, next) => {
+    const allWorks = db.getAllFromDatabase(modelWork);
+    let selectedMinionWorks = []
+    let selectedWork = {};
+    for (let i = 0; i < allWorks.length - 1; i += 1) {
+        selectedWork = allWorks[i];
+        if(selectedWork.minionId = req.params.minionId) {
+            selectedMinionWorks.push(selectedWork);
+        }
+    }
+    if(selectedMinionWorks) {
+        res.send(selectedMinionWorks);
+    } else {
+        res.status(404).send();
+    }
+});
+
+minionRouter.post('/:minionId/work', (req, res, next) => {
+    const newWork = req.body;
+    if(!newWork){  //no json body included
+        res.status(400).send();
+    } else {
+        db.addToDatabase(modelWork, newWork);
+        res.status(201).send(newWork);
+    }
+});
+
+minionRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    /*const allWorks = db.getAllFromDatabase(modelWork);
+    let selectedWork = {};
+    for (let i = 0; i < allWorks.length - 1; i += 1) {
+        selectedWork = allWorks[i];
+        if(selectedWork.minionId = req.params.minionId) {
+            break;
+        }
+    }*/
+    if(req.is('json'))
+    {
+        const upWork = db.updateInstanceInDatabase(modelWork, req.body );
+        res.send(upWork);
+    } else {
+        res.status(400).send();
+    }
+});
+
+minionRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const workId = req.params.workId;
+    const isDeleted = db.deleteFromDatabasebyId(modelWork, workId);
     if(isDeleted) { res.status(204).send() };
 });
 
